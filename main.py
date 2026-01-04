66ab4991451f85aa9ae3628e4176bc7478752c67
gr33nsquirrel <gr33n.squirr3l@gmail.com>
2026-01-04 13:06:47 +0000

    1st working code, not formatted

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from stats import get_word_count
+from stats import get_word_count, get_char_count, sort_book
 
 
-# Open and read book file 
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    word_count = get_word_count(text)
+    book_path = "books/frankenstein.txt"                    # Open book file 
+    text = get_book_text(book_path)                         # Read book file
+    word_count = get_word_count(text)                       # Get word count
+    char_count = get_char_count(text)                       # Get character count
+    book_report = sort_book(char_count)                     # Sort character count
+    print("============ BOOKBOT ============")              # Start report
+    print(f"Analyzing book found at {book_path} ...")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    print(f"{book_report}")
+    print("============= END ===============")              # End report
+    
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -1,2 +1,33 @@
+# Calculate word count
+
 def get_word_count(book):
     return len(book.split())
+
+
+# Create dictionary of character counts 
+
+def get_char_count(book):
+    char_count = {}
+
+    for c in book.lower():
+        if c in char_count:
+            char_count[c] += 1
+        else:
+            char_count[c] = 1
+    
+    return char_count
+
+
+# Sort dictionary by number of characters
+
+def sort_on_count(char_count):
+    return char_count["num"]
+
+def sort_book(report):
+    book_report = []
+    for c in report:
+       count = report[c]
+       book_report.append({"char": c, "num": count})
+    
+    book_report.sort(reverse=True,key=sort_on_count)
+    return book_report
