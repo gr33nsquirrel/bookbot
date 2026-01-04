331fb9f715771079ff6f0995de9ac51c83d63785
gr33nsquirrel <gr33n.squirr3l@gmail.com>
2026-01-04 13:56:55 +0000

    working code, formatted

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# Import modules
 from stats import get_word_count, get_char_count, sort_book
 
 
-
+# Define main
 def main():
-    book_path = "books/frankenstein.txt"                    # Open book file 
+    book_path = "books/frankenstein.txt"                    # Book filepath 
     text = get_book_text(book_path)                         # Read book file
     word_count = get_word_count(text)                       # Get word count
     char_count = get_char_count(text)                       # Get character count
@@ -13,17 +14,18 @@ def main():
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
-    print(f"{book_report}")
+    
+    for x in book_report:
+        print(f"{x['char']}: {x['num']}")
+    
     print("============= END ===============")              # End report
     
 
-
+# Read book file
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 
-
-
-
+# Call main
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,16 @@
 # Calculate word count
-
 def get_word_count(book):
     return len(book.split())
 
 
 # Create dictionary of character counts 
-
 def get_char_count(book):
     char_count = {}
 
-    for c in book.lower():
+    for c in book.lower():              # why: normalize case
+        if not c.isalpha():             # why: ignore spaces/punctuation
+            continue
+
         if c in char_count:
             char_count[c] += 1
         else:
@@ -19,7 +20,6 @@ def get_char_count(book):
 
 
 # Sort dictionary by number of characters
-
 def sort_on_count(char_count):
     return char_count["num"]
 
